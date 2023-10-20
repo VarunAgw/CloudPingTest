8563505d1b902e28a4ac72e2ea59dfb861c04d17
Varun Agrawal <Varun@VarunAgw.com>
2023-10-20 13:17:03 +0200

    Fixed bugs

--- oracle.php
+++ oracle.php
@@ -93,11 +93,11 @@ ob_start();
         }, {
             text1: "Saudi Arabia West",
             text2: "Jeddah",
-            code: "me-je
-            //     text1: "Serbia Central",
-            //     text2: "Jovanovac",
-            //     code: "eu-ddah-1"
-        // }, {jovanovac-1"
+            code: "me-jeddah-1"
+        // }, {
+        //     text1: "Serbia Central",
+        //     text2: "Jovanovac",
+        //     code: "eu-jovanovac-1"
         }, {
             text1: "Singapore",
             text2: "Singapore",
