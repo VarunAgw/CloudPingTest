40732d3ea35c0bc858e0e4e1f32ce02c26fc32a9
Varun Agrawal <Varun@VarunAgw.com>
2025-08-01 18:43:33 +0200

    Added some DO regions

--- digital_ocean.php
+++ digital_ocean.php
@@ -14,22 +14,22 @@ ob_start();
     }
 
     let regions = [{
-        //     text1: "NYC 1",
-        //     text2: "New York City",
-        //     code: "nyc-1",
-        // }, {
-        //     text1: "NYC 2",
-        //     text2: "New York City",
-        //     code: "nyc-2",
-        // }, {
+    //         text1: "NYC 1",
+    //         text2: "New York City",
+    //         code: "nyc-1",
+    // }, {
+    //     text1: "NYC 2",
+    //     text2: "New York City",
+    //     code: "nyc-2",
+    // }, {
         text1: "NYC 3",
         text2: "New York City",
         code: "nyc-3",
     }, {
-        //     text1: "SFO 1",
-        //     text2: "San Francisco",
-        //     code: "sfo-1"
-        // }, {
+    //     text1: "SFO 1",
+    //     text2: "San Francisco",
+    //     code: "sfo-1"
+    // }, {
         text1: "SFO 2",
         text2: "San Francisco",
         code: "sfo-2"
@@ -49,18 +49,18 @@ ob_start();
         text1: "SGP 1",
         text2: "Singapore",
         code: "sgp-1",
-        // }, {
-        //     text1: "LON 1",
-        //     text2: "London",
-        //     code: "lon-1"
+        }, {
+            text1: "LON 1",
+            text2: "London",
+            code: "lon-1"
     }, {
         text1: "FRA",
         text2: "Frankfurt",
         code: "fra-1"
-        // }, {
-        //     text1: "TOR 1",
-        //     text2: "Toronto",
-        //     code: "tor-1"
+        }, {
+            text1: "TOR 1",
+            text2: "Toronto",
+            code: "tor-1"
     }, {
         text1: "BLR",
         text2: "Bangalore",
